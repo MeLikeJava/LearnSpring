26127ea965ba5af97008f16c02679b958e5002cb
DESKTOP-S70PEG5\苏克山 <rerere.autoUpdate>
2021-07-05 17:35:35 +0800

    test append description

--- src/com/spring/ioc/Test.java
+++ src/com/spring/ioc/Test.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Test {
-
+        //这是新添加的主方法
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		UserService userService = (UserService)applicationContext.getBean("userService");
